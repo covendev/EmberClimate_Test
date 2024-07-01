25274f81b820700f69fbe6f44722a43257d9babf
covendev <namita1698@gmail.com>
2024-07-01 21:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import folium
 import pandas as pd
 import branca.colormap as cm
-
+from flask_frozen import Freezer
 app = Flask(__name__)
 freezer = Freezer(app)
 
